6c59037f4f66872609c572b85ca0ad70d228e02f
Geoff Vrijmoet <geofferyv@gmail.com>
2025-02-11 00:12:06 -0500

    fix: using environment variable instead of hardcoded constant for shopify store domain

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 // Shopify Configuration
-export const NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN = 'daydreamers-pet-supply.myshopify.com'
+export const NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN || ''
 export const NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN || '' 
